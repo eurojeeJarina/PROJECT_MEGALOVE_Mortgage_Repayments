8b07ceb9978e29287ec13a9887c18d12aa5b4ba7
Eurojee Jarina <44507657+eurojeeJarina@users.noreply.github.com>
2021-10-28 00:19:35 +0100

    Modified the calculateRepayments() - Interest Conversion from percentage to decimals. - Conversion of repayments.

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,21 @@
 import javax.swing.*;
 import java.awt.*;
-import java.util.Scanner;
 
 /*
-* HOW TO CALCULATE MORTGAGE REPAYMENTS:
-* M = P [i(1+i)^n]/[(1+i)^n-1]
-* */
+ * HOW TO CALCULATE MORTGAGE REPAYMENTS:
+ * M = P [i(1+i)^n]/[(1+i)^n-1]
+ * */
 
 public class Main extends JFrame {
 
 
-
     public static void main(String[] args) {
-
-
-        Scanner userInput = new Scanner(System.in);
+        //Scanner userInput = new Scanner(System.in);
+        calculateRepayments(100000,6,15);
 
         JButton calculateBtn;
         JTextField firstInputField, secondInputField;
-        JPanel
-                mainPanel;
+        JPanel mainPanel;
 
         JFrame f = new JFrame();
         mainPanel = new JPanel();
@@ -27,29 +23,32 @@ public class Main extends JFrame {
 
 
         mainPanel.add(calculateBtn);
-        mainPanel.setSize(200,220);
+        mainPanel.setSize(200, 220);
         mainPanel.setBackground(Color.BLUE);
 
         f.add(mainPanel);
-        f.setSize(400,500);
+        f.setSize(400, 500);
         f.setResizable(false);
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // terminates java programm
         f.setLayout(null);
         //f.setVisible(true);
 
     }
-    public void calculateRepayments(int loan, double ratesPercentage, int years){
+
+    public static void calculateRepayments(double loan, double ratesPercentage, int years) {
 
         int yearsToMonthsConversion = years * 12;
-        double percentToDecimalConversion = ratesPercentage / 100;
+        double percentToDecimalConversion = (ratesPercentage / 100) / 12;
 
-        int loanAmount;
-        int interestRate;
-        int numberOfPayments;
+        double loanAmount = loan;
+        double interestRate = percentToDecimalConversion;
+        int numberOfPayments = yearsToMonthsConversion;
 
-        double totalAmountOfRepayments;
+        double topEquation = ((1 + interestRate) * numberOfPayments) * interestRate;
 
-
+        System.out.println("Mortgage Amount: " + loan);
+        System.out.println("Interest Rate: " + interestRate);
+        System.out.println("Num of Payments: " + numberOfPayments);
 
     }
 
